e79840b8ad3e1849b0be7999df9c3dde92ec2f38
Daniel Levine <dlevine210@gmail.com>
2018-08-12 23:15:36 -0500

    Adding GA

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  modules: [
+    ['@nuxtjs/google-analytics', { ua: '123789022-1' }],
+  ],
   /*
   ** Customize the progress bar color
   */
